9c2dab7e94a119796761fa853f1f4c3595c0d257
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-21 15:07:35 +0100

    Create NewsSite class.

---
+++ NReaderAPI/NReaderAPI/Models/NewsSite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NReaderAPI.Models
+{
+    public class NewsSite
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        public string RSSUrl { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public NewsSite() { }
+
+        public NewsSite(string name, string RSSUrl, string logoUrl)
+        {
+            Name = name;
+            this.RSSUrl = RSSUrl;
+            LogoUrl = logoUrl;
+        }
+    }
+}
