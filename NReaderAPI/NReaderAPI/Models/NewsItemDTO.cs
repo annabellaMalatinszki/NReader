74196d75cea9c1a5e45c8d192f7ccd8ec2c146a5
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-21 18:38:20 +0100

    Create DTO for the NewsItem class

---
+++ NReaderAPI/NReaderAPI/Models/NewsItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NReaderAPI.Models
+{
+    public class NewsItemDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string NewsSiteName { get; set; }
+    }
+}
