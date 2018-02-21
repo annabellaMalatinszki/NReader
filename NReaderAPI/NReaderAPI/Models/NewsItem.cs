b8297895e188280ad78e2c6224977792a5b54729
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-21 15:06:17 +0100

    Create NewsItem class.

---
+++ NReaderAPI/NReaderAPI/Models/NewsItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace NReaderAPI.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string PublicationDate { get; set; }
+
+        public string ArticleUrl { get; set; }
+
+        public string PicUrl { get; set; }
+
+        public NewsItem() { }
+
+        public NewsItem(string title, string description, string publicationDate, string articleUrl, string picUrl)
+        {
+            Title = title;
+            Description = description;
+            PublicationDate = ParseDate(publicationDate);
+            ArticleUrl = articleUrl;
+            PicUrl = picUrl;
+        }
+
+        public static string ParseDate(string date)
+        {
+            DateTime parsedDate;
+            string pattern = "ddd, dd MMM yyyy HH:mm:ss GMT";
+            string pattern2 = "ddd, dd MMM yyyy HH:mm:ss";
+
+            if (DateTime.TryParseExact(date, pattern, null, DateTimeStyles.None, out parsedDate))
+            {
+                return parsedDate.ToString();
+            }
+            else if (DateTime.TryParseExact(date, pattern2, null, DateTimeStyles.None, out parsedDate))
+            {
+                return parsedDate.ToString();
+            }
+
+            return DateTime.Now.ToString();
+        }
+
+
+    }
+}
