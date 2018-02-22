469428b6abede31f247a8e6edb774e9eddd368a5
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-22 22:25:16 +0100

    Implement ParallelRSSDownloadAsync to retrieve NewsItem objects from NewsSites

--- NReaderAPI/NReaderAPI/Models/RSSParser.cs
+++ NReaderAPI/NReaderAPI/Models/RSSParser.cs
@@ -12,6 +12,48 @@ namespace NReaderAPI.Models
         // The RSSParser is a singleton.
         static RSSParser() { }
 
-        
+        // Call this method to retrieve NewsItem objects form NewsSites
+        public static async Task ParallelRSSDownloadAsync()
+        {
+            List<string> urls = GetUrls();
+
+            // Retrieve NewsItem objects from xml asynchronously.
+            List<Task<List<NewsItem>>> tasks = new List<Task<List<NewsItem>>>();
+            foreach (string url in urls)
+            {
+                tasks.Add(Task.Run(() => GetNewsItemsFromRSSAsync(url)));
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            List<Task<string>> tasks2 = new List<Task<string>>();
+
+            foreach (var resultList in results)
+            {
+                //Here you call the function that will add newsitems to the database.
+                tasks2.Add(Task.Run(() => InsertToDatabaseAsync(resultList)));
+            }
+
+            var results2 = await Task.WhenAll(tasks2);
+        }
+
+        static void GetNewsItemsFromRSSAsync(string url)
+        {
+            // Parse the file at the url and create NewsItem object from the xml.
+            
+        }
+
+        private static void GetUrls()
+        {
+            // TODO: Here you should retrieve the urls of the sites that are to be checked from the database.
+;
+        }
+
+        static async void InsertToDatabaseAsync(List<NewsItem> newsList)
+        {
+            // TODO: Here you should put the new NewsItems into the database.
+
+        }
+
     }
 }
