524b1dba106924c5bab8e7dcae16b0883d63bdea
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-22 22:22:06 +0100

    Make the RSSParser class a singleton

--- NReaderAPI/NReaderAPI/Models/RSSParser.cs
+++ NReaderAPI/NReaderAPI/Models/RSSParser.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using System.Xml;
 
 namespace NReaderAPI.Models
 {
     public class RSSParser
     {
+        // The RSSParser is a singleton.
+        static RSSParser() { }
+
+        
     }
 }
