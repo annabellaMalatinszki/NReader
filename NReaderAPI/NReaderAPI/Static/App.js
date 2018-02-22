3c89bf4bb87dac5edeb7265e0cd371df548dec2a
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-22 15:36:59 +0100

    Create AJAX request sending in the Controller js class

--- NReaderAPI/NReaderAPI/Static/App.js
+++ NReaderAPI/NReaderAPI/Static/App.js
@@ -1,3 +1,41 @@
-﻿const view = new View();
+﻿class View {
+
+    constructor() {
+        this.$newsItems = document.querySelector('#news-items');
+        this.$newsfeed = document.querySelector('#newsfeed');
+    }
+
+}
+
+class Controller {
+
+    constructor(view) {
+        Controller.GET = 'GET';
+        Controller.POST = 'POST';
+
+        this.view = view;
+
+        // Bindings to the view should go here.
+
+        this.load();
+    }
+
+    sendAjax(endpoint, method, params = null, onSuccess = null) {
+        const scope = this;
+        const req = new XMLHttpRequest();
+        req.addEventListener("load", function (event) {
+            onSuccess.call(scope, event);
+        });
+        req.open(method, endpoint);
+        if (method === Controller.POST) {
+            req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        }
+        req.send(params);
+    }
+
+
+}
+
+const view = new View();
 
 new Controller(view);
