60f51a626ad503f2488b8e1fdc1a007928dd8b36
Annabella Malatinszki <a.malatinszki@gmail.com>
2018-03-08 20:59:18 +0100

    Add idCounter static field to the NewsSite class to increment ids

--- NReaderAPI/NReaderAPI/Models/NewsSite.cs
+++ NReaderAPI/NReaderAPI/Models/NewsSite.cs
@@ -8,6 +8,7 @@ namespace NReaderAPI.Models
 {
     public class NewsSite
     {
+        private static int idCounter = 0;
         public int Id { get; set; }
         [Required]
 
@@ -17,10 +18,15 @@ namespace NReaderAPI.Models
 
         public string LogoUrl { get; set; }
 
-        public NewsSite() { }
+        public NewsSite() {
+            Id = idCounter;
+            idCounter++;
+        }
 
         public NewsSite(string name, string RSSUrl, string logoUrl)
         {
+            Id = idCounter;
+            idCounter++;
             Name = name;
             this.RSSUrl = RSSUrl;
             LogoUrl = logoUrl;
