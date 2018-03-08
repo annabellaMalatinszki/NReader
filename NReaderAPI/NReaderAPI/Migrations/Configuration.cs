fb9abd7c6e1991aeb884afcc74dc55a2482ab4ca
Annabella Malatinszki <a.malatinszki@gmail.com>
2018-03-08 21:02:08 +0100

    Migrate NewsSite data to the database

--- NReaderAPI/NReaderAPI/Migrations/Configuration.cs
+++ NReaderAPI/NReaderAPI/Migrations/Configuration.cs
@@ -27,6 +27,16 @@
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            context.NewsSites.AddOrUpdate(n => n.Id,
+                new NewsSite() { Name = "Index", RSSUrl = "https://index.hu/24ora/rss/", LogoUrl = "https://seeklogo.com/images/I/index-logo-72A9CE114B-seeklogo.com.gif" },
+                new NewsSite() { Name = "HVG", RSSUrl = "http://hvg.hu/rss", LogoUrl = "https://pbs.twimg.com/profile_images/721846144788094976/YCrswAJy_400x400.jpg" },
+                new NewsSite() { Name = "444", RSSUrl = "https://444.hu/feed", LogoUrl = "https://pbs.twimg.com/profile_images/3618561514/070a085e979275ddf653929d353ba09b_400x400.png" },
+                new NewsSite() { Name = "Origo", RSSUrl = "http://www.origo.hu/contentpartner/rss/origoall/origo.xml", LogoUrl = "http://cdn.nwmgroups.hu/s/20130604/img/cimlap2013/origo-social.png" },
+                new NewsSite() { Name = "Magyar Nemzet Online", RSSUrl = "https://mno.hu/rss", LogoUrl = "http://mno.hu/static/frontend/imgs/logo2.png" },
+                new NewsSite() { Name = "24.hu", RSSUrl = "https://24.hu/feed/", LogoUrl = "https://www.rendi.hu/images/24.png" },
+                new NewsSite() { Name = "888", RSSUrl = "https://888.hu/rss/", LogoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/9Fi27_Gp.jpg/200px-9Fi27_Gp.jpg" },
+                new NewsSite() { Name = "Pesti Srácok", RSSUrl = "https://pestisracok.hu/feed/", LogoUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHqaY-0PmTiCuEIopr51kUlhEGNaDPiaznKF-1Rnc4Uk3kasVh" }
+                );
 
         }
     }
