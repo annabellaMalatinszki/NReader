a55fcaf1dc80e4866b4d54166159b25cdb6451f5
Annabella Malatinszki <a.malatinszki@gmail.com>
2018-03-09 03:15:53 +0100

    Play around with react rendering

---
+++ NReaderAPI/NReaderAPI/Scripts/App.jsx
@@ -0,0 +1,12 @@
+﻿
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App" >
+                Hello, world! I am an App.
+            </div>)
+    }
+}
+
+export default App;
---
+++ NReaderAPI/NReaderAPI/Scripts/index.jsx
@@ -0,0 +1,17 @@
+﻿//import React from 'react';
+//import { render } from 'react-dom';
+//import { App } from './App';
+
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App" >
+                Hello, world! I am an App.
+            </div>)
+    }
+}
+
+ReactDOM.render( <App />, document.querySelector('#content'));
